eab028a71a63f9838189dc62e460096a54aad442
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-03-15 14:40:23 -0700

    commit tree 82cc297433838d41adcfb3f4890669e3cb676368

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Nonfiction) }
+    validate :title_must_be_clickbate
+
+    def title_must_be_clickbate
+        if !!self.title
+            if !self.title.match?(/(Won't Believe)|(Secret)|(Top \d)|(Guess)/)
+                errors.add(:title, "must be clickbait-y") 
+            end
+        end
+    end
 end
+
+  
